889f3bab6920a3caf186de1f82dfefee39e17422
Priscilla H <priscillamhuynh@gmail.com>
2018-07-02 11:48:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,11 @@ class Song
   end
 
   def self.artist_count
-    @@artists
+    artist_count={}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist]+=1
+      else
+        artist_count[artist]=1
   end
 end
